794b8afaeacbec75437b3d03743c1fabd0b2cb03
Jakub Półgęsek <jakub@polgesek.pl>
2019-04-08 15:14:14 +0200

    Add test js - example parsing data from lightDM object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,13 @@
 var app = {
+	debug: false,
+	
 	init: function(){
+		if (typeof window.lightdm == "undefined"){
+			window.lightdm = app.debug.fakeLightDM;
+		}
+
+		app.ui.init();
+
 		return true;
 	}
 };
--- assets/js/app.ui.js
+++ assets/js/app.ui.js
@@ -1,3 +1,14 @@
 app.ui = {
+	elements: {
+		container: document.getElementById("container")
+	},
 
+	init: function(){
+		app.ui.elements.container.innerHTML = "<h1>Hello World!</h1>";
+		app.ui.elements.container.innerHTML += "I am running on: <b>" + window.lightdm.hostname + "</b><br>";
+		app.ui.elements.container.innerHTML += "Available users: <b>" + window.lightdm.users.join(", ") + "</b><br>";
+		app.ui.elements.container.innerHTML += "Available sessions: <b>" + window.lightdm.sessions.join(", ") + "</b><br>";
+		app.ui.elements.container.innerHTML += "Available keymaps: <b>" + window.lightdm.layouts.join(", ") + "</b><br>";
+		app.ui.elements.container.innerHTML += "Available languages: <b>" + window.lightdm.languages.join(", ") + "</b><br>";
+	}
 };
