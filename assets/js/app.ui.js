813ae94613e474823aec3dd4dd69425a422957f9
Jakub Półgęsek <jakub@polgesek.pl>
2019-04-08 15:33:30 +0200

    Fix dumpper to support more objects

--- assets/js/app.ui.js
+++ assets/js/app.ui.js
@@ -6,13 +6,29 @@ app.ui = {
 	init: function(){
 		app.ui.elements.container.innerHTML = "<h1>Hello World!</h1>";
 		app.ui.elements.container.innerHTML += "I am running on: <b>" + window.lightdm.hostname + "</b><br>";
+		
+		app.ui.elements.container.innerHTML += "<hr>";
 
 		Object.keys(window.lightdm.users).forEach(key => {
 			app.ui.elements.container.innerHTML += "Available user: <b>" + JSON.stringify(window.lightdm.users[key]) + "</b><br>";
 		})
 		
-		app.ui.elements.container.innerHTML += "Available sessions: <b>" + window.lightdm.sessions.join(", ") + "</b><br>";
-		app.ui.elements.container.innerHTML += "Available keymaps: <b>" + window.lightdm.layouts.join(", ") + "</b><br>";
-		app.ui.elements.container.innerHTML += "Available languages: <b>" + window.lightdm.languages.join(", ") + "</b><br>";
+		app.ui.elements.container.innerHTML += "<hr>";
+
+		Object.keys(window.lightdm.sessions).forEach(key => {
+			app.ui.elements.container.innerHTML += "Available session: <b>" + JSON.stringify(window.lightdm.sessions[key]) + "</b><br>";
+		})
+		
+		app.ui.elements.container.innerHTML += "<hr>";
+
+		Object.keys(window.lightdm.layouts).forEach(key => {
+			app.ui.elements.container.innerHTML += "Available keymap: <b>" + JSON.stringify(window.lightdm.layouts[key]) + "</b><br>";
+		})
+		
+		app.ui.elements.container.innerHTML += "<hr>";
+
+		Object.keys(window.lightdm.languages).forEach(key => {
+			app.ui.elements.container.innerHTML += "Available language: <b>" + JSON.stringify(window.lightdm.languages[key]) + "</b><br>";
+		})
 	}
 };
